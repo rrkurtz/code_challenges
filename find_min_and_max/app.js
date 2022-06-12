6191d0a0f0ebe272dbdd9f4b472a91ad1f94a36e
robinkurtz@me.com <robinkurtz@me.com>
2022-06-11 20:13:02 -0500

    codewars

---
+++ find_min_and_max/app.js
@@ -0,0 +1,9 @@
+//Your task is to make two functions (max and min, or maximum and minimum, etc., depending on the language) that receive a list of integers as input and return, respectively, the largest and lowest number in that list.
+
+let min = function(list){
+    return Math.min(...list);
+}
+
+let max = function(list){
+   return Math.max(...list);
+}
--- simple_multiplication/app.js
+++ simple_multiplication/app.js
@@ -1,7 +1,5 @@
 //This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
 
-
-
 function simpleMultiplication(number) {
     if(number % 2 === 0){
       return number * 8
